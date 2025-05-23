670d774b875af5eace32a8a977d8ec98f81ee495
Nandini Deodia <nandinideodia.2406@gmail.com>
2025-05-23 15:32:12 +0530

    Name validation

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeeRequestDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeeRequestDTO.java
@@ -2,9 +2,16 @@ package com.bridgelabz.employeepayroll.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record EmployeeRequestDTO(
-    @NotBlank String name,
+	@NotBlank(message = "Employee name is mandatory")
+	@Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+	@Pattern(
+	        regexp = "^[\\p{L} .'-]+$", 
+	        message = "Name can only contain letters, spaces, apostrophes, hyphens, and dots") 
+	String name,
     @Min(1000) double salary,
     String department
 ) {
